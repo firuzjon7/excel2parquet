b69464d0f4c2a97ff995216afcda5ba70d6f5537
Firuzjon Kurbonov <firuzjon.kurbonov@bayer.com>
2022-07-18 13:50:47 +0200

    Add more functionality to read files from cwd

--- test_pa.py
+++ test_pa.py
@@ -2,7 +2,7 @@ from fileinput import filename
 import pyarrow as pv
 import pyarrow.parquet as pq
 import pandas as pd
-
+import os
 
 
 
@@ -16,13 +16,24 @@ class parquetFile (object):
         table = pv.Table.from_pandas(data)
         pq.write_table(table, self.file+'.parquet')
 
-file = 'test_schema2.xlsx'
-sheet = 'Tabelle1'
+
+current_dir = os.getcwd()
+list_of_files = os.listdir(current_dir)
 
 
+file_sheet = []
 
-first_file = parquetFile(file, sheet)
+for file in list_of_files:
+    if file.lower().endswith((".xlsx",".xls")):
+        sheets = pd.ExcelFile(file).sheet_names
+        for sheet in sheets:
+            file_sheet.extend([{file : sheet}])
 
-first_file.excel_to_parquet()
+
+for index, i in enumerate(file_sheet):
+        for file_name in i.keys():
+            for sheet_name in i.values():
+                first_file = parquetFile(file_name, sheet_name)
+                first_file.excel_to_parquet()
 
 
